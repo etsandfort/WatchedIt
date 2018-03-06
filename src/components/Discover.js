5b3dd85b6b74a52a3d5aed66d8b9d5f8a94cf91e
davidmikh <david.mikhayelyan@outlook.com>
2018-03-05 22:45:58 -0600

    created basic discover page with monkaS

---
+++ src/components/Discover.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import logo from '../logo.svg';
+import './includes/App.css';
+
+class Discover extends Component {
+  render() {
+    return (
+      <div className="Discover">
+        <table className="table">
+          <tr>
+            <td><button className="btn btn-link">Trending</button></td>
+            <td><button className="btn btn-link">New Releases</button></td>
+            <td><button className="btn btn-link">Genres</button></td>
+          </tr>
+        </table>
+        <table className="table">
+          <tr>
+            <td><button className="btn btn-link">Netflix</button></td>
+          </tr>
+        </table>
+        <table className="table">
+          <tr>
+            <td><button class="btn btn-primary btn-lg btn-block"><img src="http://i3.kym-cdn.com/entries/icons/original/000/022/713/4.png"/></button></td>
+            <td><button class="btn btn-primary btn-lg btn-block"><img src="http://i3.kym-cdn.com/entries/icons/original/000/022/713/4.png"/></button></td>
+            <td><button class="btn btn-primary btn-lg btn-block"><img src="http://i3.kym-cdn.com/entries/icons/original/000/022/713/4.png"/></button></td>
+            <td><button class="btn btn-primary btn-lg btn-block"><img src="http://i3.kym-cdn.com/entries/icons/original/000/022/713/4.png"/></button></td>
+            <td><button class="btn btn-primary btn-lg btn-block"><img src="http://i3.kym-cdn.com/entries/icons/original/000/022/713/4.png"/></button></td>
+            <td><button class="btn btn-primary btn-lg btn-block"><img src="http://i3.kym-cdn.com/entries/icons/original/000/022/713/4.png"/></button></td>
+          </tr>
+        </table>
+        <table className="table">
+          <tr>
+            <td><button className="btn btn-link">Amazon</button></td>
+          </tr>
+        </table>
+        <table className="table">
+          <tr>
+            <td><button class="btn btn-primary btn-lg btn-block">TITLE</button></td>
+            <td><button class="btn btn-primary btn-lg btn-block">TITLE</button></td>
+            <td><button class="btn btn-primary btn-lg btn-block">TITLE</button></td>
+            <td><button class="btn btn-primary btn-lg btn-block">TITLE</button></td>
+            <td><button class="btn btn-primary btn-lg btn-block">TITLE</button></td>
+            <td><button class="btn btn-primary btn-lg btn-block">TITLE</button></td>
+          </tr>
+        </table>
+        <table className="table">
+          <tr>
+            <td><button className="btn btn-link">HBO</button></td>
+          </tr>
+        </table>
+        <table className="table">
+          <tr>
+            <td><button class="btn btn-primary btn-lg btn-block">TITLE</button></td>
+            <td><button class="btn btn-primary btn-lg btn-block">TITLE</button></td>
+            <td><button class="btn btn-primary btn-lg btn-block">TITLE</button></td>
+            <td><button class="btn btn-primary btn-lg btn-block">TITLE</button></td>
+            <td><button class="btn btn-primary btn-lg btn-block">TITLE</button></td>
+            <td><button class="btn btn-primary btn-lg btn-block">TITLE</button></td>
+          </tr>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default Discover;
