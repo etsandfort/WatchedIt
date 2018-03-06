caad7fb86db9f143bb00f13e64160b23a9d25de0
mattkachek <mattkachek@users.noreply.github.com>
2018-03-05 22:53:38 -0600

    Adds the sidebar
    Doesn't break everything this time

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,16 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
+import Sidebar from './Sidebar.js';
 import './includes/App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          Hello World
-        </p>
-      </div>
+      <body className="App">
+		<div className="App-Sidebar">
+			<Sidebar/>
+		</div>
+      </body>
     );
   }
 }
