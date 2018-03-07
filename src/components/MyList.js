69b31376c4808673be30f8b766000e3a25222f83
Adam Fitzgibbon <fitzgamer@hotmail.com>
2018-03-06 23:24:08 -0600

    Added react-table and example

--- src/components/MyList.js
+++ src/components/MyList.js
@@ -1,53 +1,73 @@
 import React, { Component } from 'react';
 import './includes/App.css';
+import ReactTable from 'react-table'
 
 class MyList extends Component {
   render() {
+    const data = [{
+      name: 'Bob Ross',
+      age: 46,
+      friend: {
+        name: 'Kappa Ross',
+        age: 13,
+      }
+    },
+    {
+      name: 'Dans Gaming',
+      age: 26,
+      friend: {
+        name: 'Pog Champ',
+        age: 23,
+      }
+    },
+    {
+      name: 'Jotaro Joestar',
+      age: 16,
+      friend: {
+        name: 'Star Platinum',
+        age: 20,
+      }
+    }];
+  
+    const columns = [{
+      Header: 'Name',
+      accessor: 'name' // String-based value accessors!
+    }, {
+      Header: 'Age',
+      accessor: 'age',
+      Cell: props => <span className='number'>{props.value}</span> // Custom cell components!
+    }, {
+      id: 'friendName', // Required because our accessor is not a string
+      Header: 'Friend Name',
+      accessor: d => d.friend.name // Custom value accessors!
+    }, {
+      Header: props => <span>Friend Age</span>, // Custom header components!
+      accessor: 'friend.age'
+    }]
     return (
       <div className="MyList container">
-        <header className="row">
+      <header className="row">
           <h1 className="App-title">MyList</h1>
         </header>
         <div className="row">
-          <button className="col-sm-4 btn btn-primary">
-            Add a Show/Movie
-          </button>
-          <button className="col-sm-4 offset-sm-4 btn btn-info">
-            Advanced Search
-          </button>
+          <div className="col-sm-4 control-group">
+            <div className="btn-toolbar">
+            <button className="btn btn-primary">
+              Add a Show/Movie
+            </button>
+            </div>
+          </div>
+          <div className="col-sm-4 offset-sm-4">
+            <div className="btn-toolbar">
+            <button className="btn btn-info">
+              Advanced Search
+            </button>
+            </div>
+          </div>
         </div>
-        <div className="row">
-          <table class="table table-dark">
-            <thead>
-              <tr>
-                <th>#</th>
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>Username</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-              </tr>
-              <tr>
-                <th scope="row">2</th>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-              </tr>
-              <tr>
-                <th scope="row">3</th>
-                <td>Larry</td>
-                <td>the Bird</td>
-                <td>@twitter</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+        <ReactTable
+          data={data}
+          columns={columns} />
       </div>
     );
   }
