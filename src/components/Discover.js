418ef5e25c7960da26e0fb6354eeb34436445ac8
Grant Fishurn <granttfish@gmail.com>
2018-03-09 17:24:11 -0600

    Fixed table errors

--- src/components/Discover.js
+++ src/components/Discover.js
@@ -17,36 +17,42 @@ class Discover extends Component {
 		</div>
           <h2 style={{paddingLeft: 15, padding: 10}} align="left">Netflix</h2>
         <table className="table">
-          <tr>
-            <td><div className="Discover-Title-Card"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SY1000_CR0,0,641,1000_AL_.jpg"/></div></td>
-            <td><div className="Discover-Title-Card"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BMDA2YTQzNTktNzA5Yi00Mzg2LWE1NzMtZTQxYTBhNDVjYTAxXkEyXkFqcGdeQXVyMjM5ODMxODc@._V1_SY1000_CR0,0,709,1000_AL_.jpg"/></div></td>
-            <td><div className="Discover-Title-Card"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BMDA2YTQzNTktNzA5Yi00Mzg2LWE1NzMtZTQxYTBhNDVjYTAxXkEyXkFqcGdeQXVyMjM5ODMxODc@._V1_SY1000_CR0,0,709,1000_AL_.jpg"/></div></td>
-            <td><div className="Discover-Title-Card"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BMDA2YTQzNTktNzA5Yi00Mzg2LWE1NzMtZTQxYTBhNDVjYTAxXkEyXkFqcGdeQXVyMjM5ODMxODc@._V1_SY1000_CR0,0,709,1000_AL_.jpg"/></div></td>
-            <td><div className="Discover-Title-Card"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BMDA2YTQzNTktNzA5Yi00Mzg2LWE1NzMtZTQxYTBhNDVjYTAxXkEyXkFqcGdeQXVyMjM5ODMxODc@._V1_SY1000_CR0,0,709,1000_AL_.jpg"/></div></td>
-			<td><div className="Discover-Title-Card"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BMDA2YTQzNTktNzA5Yi00Mzg2LWE1NzMtZTQxYTBhNDVjYTAxXkEyXkFqcGdeQXVyMjM5ODMxODc@._V1_SY1000_CR0,0,709,1000_AL_.jpg"/></div></td>
-          </tr>
+			<tbody>
+				<tr>
+					<td><div className="Discover-Title-Card"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SY1000_CR0,0,641,1000_AL_.jpg"/></div></td>
+					<td><div className="Discover-Title-Card"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BMDA2YTQzNTktNzA5Yi00Mzg2LWE1NzMtZTQxYTBhNDVjYTAxXkEyXkFqcGdeQXVyMjM5ODMxODc@._V1_SY1000_CR0,0,709,1000_AL_.jpg"/></div></td>
+					<td><div className="Discover-Title-Card"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BMDA2YTQzNTktNzA5Yi00Mzg2LWE1NzMtZTQxYTBhNDVjYTAxXkEyXkFqcGdeQXVyMjM5ODMxODc@._V1_SY1000_CR0,0,709,1000_AL_.jpg"/></div></td>
+					<td><div className="Discover-Title-Card"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BMDA2YTQzNTktNzA5Yi00Mzg2LWE1NzMtZTQxYTBhNDVjYTAxXkEyXkFqcGdeQXVyMjM5ODMxODc@._V1_SY1000_CR0,0,709,1000_AL_.jpg"/></div></td>
+					<td><div className="Discover-Title-Card"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BMDA2YTQzNTktNzA5Yi00Mzg2LWE1NzMtZTQxYTBhNDVjYTAxXkEyXkFqcGdeQXVyMjM5ODMxODc@._V1_SY1000_CR0,0,709,1000_AL_.jpg"/></div></td>
+					<td><div className="Discover-Title-Card"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BMDA2YTQzNTktNzA5Yi00Mzg2LWE1NzMtZTQxYTBhNDVjYTAxXkEyXkFqcGdeQXVyMjM5ODMxODc@._V1_SY1000_CR0,0,709,1000_AL_.jpg"/></div></td>
+				</tr>
+			</tbody>
         </table>
           <h2 style={{paddingLeft: 15}} align="left">Amazon</h2>
         <table className="table">
-          <tr>
-            <td><div className="Discover-Title-Card"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SY1000_CR0,0,641,1000_AL_.jpg"/></div></td>
-            <td><div className="Discover-Title-Card"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BMDA2YTQzNTktNzA5Yi00Mzg2LWE1NzMtZTQxYTBhNDVjYTAxXkEyXkFqcGdeQXVyMjM5ODMxODc@._V1_SY1000_CR0,0,709,1000_AL_.jpg"/></div></td>
-            <td><div className="Discover-Title-Card"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BMDA2YTQzNTktNzA5Yi00Mzg2LWE1NzMtZTQxYTBhNDVjYTAxXkEyXkFqcGdeQXVyMjM5ODMxODc@._V1_SY1000_CR0,0,709,1000_AL_.jpg"/></div></td>
-            <td><div className="Discover-Title-Card"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BMDA2YTQzNTktNzA5Yi00Mzg2LWE1NzMtZTQxYTBhNDVjYTAxXkEyXkFqcGdeQXVyMjM5ODMxODc@._V1_SY1000_CR0,0,709,1000_AL_.jpg"/></div></td>
-            <td><div className="Discover-Title-Card"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BMDA2YTQzNTktNzA5Yi00Mzg2LWE1NzMtZTQxYTBhNDVjYTAxXkEyXkFqcGdeQXVyMjM5ODMxODc@._V1_SY1000_CR0,0,709,1000_AL_.jpg"/></div></td>
-			<td><div className="Discover-Title-Card"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BMDA2YTQzNTktNzA5Yi00Mzg2LWE1NzMtZTQxYTBhNDVjYTAxXkEyXkFqcGdeQXVyMjM5ODMxODc@._V1_SY1000_CR0,0,709,1000_AL_.jpg"/></div></td>
-          </tr>
+			<tbody>
+				<tr>
+					<td><div className="Discover-Title-Card"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SY1000_CR0,0,641,1000_AL_.jpg"/></div></td>
+					<td><div className="Discover-Title-Card"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BMDA2YTQzNTktNzA5Yi00Mzg2LWE1NzMtZTQxYTBhNDVjYTAxXkEyXkFqcGdeQXVyMjM5ODMxODc@._V1_SY1000_CR0,0,709,1000_AL_.jpg"/></div></td>
+					<td><div className="Discover-Title-Card"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BMDA2YTQzNTktNzA5Yi00Mzg2LWE1NzMtZTQxYTBhNDVjYTAxXkEyXkFqcGdeQXVyMjM5ODMxODc@._V1_SY1000_CR0,0,709,1000_AL_.jpg"/></div></td>
+					<td><div className="Discover-Title-Card"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BMDA2YTQzNTktNzA5Yi00Mzg2LWE1NzMtZTQxYTBhNDVjYTAxXkEyXkFqcGdeQXVyMjM5ODMxODc@._V1_SY1000_CR0,0,709,1000_AL_.jpg"/></div></td>
+					<td><div className="Discover-Title-Card"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BMDA2YTQzNTktNzA5Yi00Mzg2LWE1NzMtZTQxYTBhNDVjYTAxXkEyXkFqcGdeQXVyMjM5ODMxODc@._V1_SY1000_CR0,0,709,1000_AL_.jpg"/></div></td>
+					<td><div className="Discover-Title-Card"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BMDA2YTQzNTktNzA5Yi00Mzg2LWE1NzMtZTQxYTBhNDVjYTAxXkEyXkFqcGdeQXVyMjM5ODMxODc@._V1_SY1000_CR0,0,709,1000_AL_.jpg"/></div></td>
+				</tr>
+		  </tbody>
         </table>
           <h2 style={{paddingLeft: 15}} align="left">HBO</h2>
         <table className="table">
-          <tr>
-            <td><div className="Discover-Title-Card"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SY1000_CR0,0,641,1000_AL_.jpg"/></div></td>
-            <td><div className="Discover-Title-Card"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BMDA2YTQzNTktNzA5Yi00Mzg2LWE1NzMtZTQxYTBhNDVjYTAxXkEyXkFqcGdeQXVyMjM5ODMxODc@._V1_SY1000_CR0,0,709,1000_AL_.jpg"/></div></td>
-            <td><div className="Discover-Title-Card"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BMDA2YTQzNTktNzA5Yi00Mzg2LWE1NzMtZTQxYTBhNDVjYTAxXkEyXkFqcGdeQXVyMjM5ODMxODc@._V1_SY1000_CR0,0,709,1000_AL_.jpg"/></div></td>
-            <td><div className="Discover-Title-Card"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BMDA2YTQzNTktNzA5Yi00Mzg2LWE1NzMtZTQxYTBhNDVjYTAxXkEyXkFqcGdeQXVyMjM5ODMxODc@._V1_SY1000_CR0,0,709,1000_AL_.jpg"/></div></td>
-            <td><div className="Discover-Title-Card"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BMDA2YTQzNTktNzA5Yi00Mzg2LWE1NzMtZTQxYTBhNDVjYTAxXkEyXkFqcGdeQXVyMjM5ODMxODc@._V1_SY1000_CR0,0,709,1000_AL_.jpg"/></div></td>
-			<td><div className="Discover-Title-Card"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BMDA2YTQzNTktNzA5Yi00Mzg2LWE1NzMtZTQxYTBhNDVjYTAxXkEyXkFqcGdeQXVyMjM5ODMxODc@._V1_SY1000_CR0,0,709,1000_AL_.jpg"/></div></td>
-          </tr>
+			<tbody>
+				<tr>
+					<td><div className="Discover-Title-Card"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SY1000_CR0,0,641,1000_AL_.jpg"/></div></td>
+					<td><div className="Discover-Title-Card"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BMDA2YTQzNTktNzA5Yi00Mzg2LWE1NzMtZTQxYTBhNDVjYTAxXkEyXkFqcGdeQXVyMjM5ODMxODc@._V1_SY1000_CR0,0,709,1000_AL_.jpg"/></div></td>
+					<td><div className="Discover-Title-Card"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BMDA2YTQzNTktNzA5Yi00Mzg2LWE1NzMtZTQxYTBhNDVjYTAxXkEyXkFqcGdeQXVyMjM5ODMxODc@._V1_SY1000_CR0,0,709,1000_AL_.jpg"/></div></td>
+					<td><div className="Discover-Title-Card"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BMDA2YTQzNTktNzA5Yi00Mzg2LWE1NzMtZTQxYTBhNDVjYTAxXkEyXkFqcGdeQXVyMjM5ODMxODc@._V1_SY1000_CR0,0,709,1000_AL_.jpg"/></div></td>
+					<td><div className="Discover-Title-Card"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BMDA2YTQzNTktNzA5Yi00Mzg2LWE1NzMtZTQxYTBhNDVjYTAxXkEyXkFqcGdeQXVyMjM5ODMxODc@._V1_SY1000_CR0,0,709,1000_AL_.jpg"/></div></td>
+					<td><div className="Discover-Title-Card"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BMDA2YTQzNTktNzA5Yi00Mzg2LWE1NzMtZTQxYTBhNDVjYTAxXkEyXkFqcGdeQXVyMjM5ODMxODc@._V1_SY1000_CR0,0,709,1000_AL_.jpg"/></div></td>
+				</tr>
+			</tbody>
         </table>
         </div>
         </div>
